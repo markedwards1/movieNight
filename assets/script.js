36cb3bac1a4cdbf905c248d74f09c4e9ba421724
Mark Edwards <markedwards@Marks-MBP.lan>
2022-06-23 20:50:36 +0800

    js changes

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,49 @@
 //user is presented combo result page
-const movieTitle = document.getElementById('movie');
-const moviePic = document.getElementById('moviePic');
+const movieTitle = document.getElementById('movieTitle');
+const moviePicContainer = document.getElementById('moviePic');
 const review = document.getElementById('review')
 const comboTitle = document.getElementById('snackTitle')
 const comboPic = document.getElementById('snackPic');
 const ingredients = document.getElementById('ingredients');
 
 
+//create random number
+const number = 1000;
+const movieNumber = Math.floor(Math.random()* number);
+//console.log(movieNumber);
+getApiMovie();
 
 //fetch movie
+//insert random number into url to search the movies. 
+function getApiMovie(){
+    let requestUrl = 'https://api.themoviedb.org/3/movie/' + movieNumber + '?api_key=417ba82f420aac26f214a4ce75d520d6';
+   console.log(requestUrl);
+   fetch(requestUrl)
+    .then(function (response){
+        return response.json();
+    })
+    .then(function (data){
+        console.log(data);
+    
+       movieTitle.textContent = data.original_title;
+        review.textContent = data.overview;
 
+
+
+
+
+            const imgURL = data.poster_path;
+            const movieImg = document.createElement('img')
+            movieImg.getAttribute("src", imgURL)
+            moviePicContainer.appendChild(movieImg);
+
+
+
+
+            
+    })
+
+}
 //display inforamtion using ID's
 
 //fetch the food.
