4a851694c6aa0a34b52283cf7ad53d49c3c8cbcc
Mark Edwards <markedwards@Marks-MBP.lan>
2022-06-27 21:44:56 +0800

    test

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,14 @@
 //user is presented combo result page
 const movieTitle = document.getElementById('movieTitle');
 const moviePicContainer = document.getElementById('moviePic');
-const review = document.getElementById('review')
-const comboTitle = document.getElementById('snackTitle')
+const review = document.getElementById('review');
+const comboTitle = document.getElementById('snackTitle');
 const comboPic = document.getElementById('snackPic');
 const ingredients = document.getElementById('ingredients');
 
+const generate = document.getElementById('generate');
+
+ generate.addEventListener('click', generates());
 
 //create random number
 const number = 777281;
@@ -14,42 +17,83 @@ const movieNumber = Math.floor(Math.random()* number);
 getApiMovie();
 
 //fetch movie
-//insert random number into url to search the movies. ...
+//insert random number into url to search the movies. 
 function getApiMovie(){
+    let movieNumber = Math.floor(Math.random()* number);
     let requestUrl = 'https://api.themoviedb.org/3/movie/' + movieNumber + '?api_key=417ba82f420aac26f214a4ce75d520d6';
-   console.log(requestUrl);
-   fetch(requestUrl)
+    console.log(requestUrl);
+    fetch(requestUrl)
     .then(function (response){
+        
+        
         return response.json();
     })
     .then(function (data){
         console.log(data);
-    
-       movieTitle.textContent = data.original_title;
-
-       review.textContent = data.overview;
-
+        
+        movieTitle.textContent = data.original_title;
+        review.textContent = data.overview;
+        
+        if(data.adult === true){
+            location.reload();
+        }
+        
+        if(data.success === false){
+           
+            location.reload();
+        }
+        if(data.poster_path === null){
+            location.reload();
+        }
         
 
-
-
-            const imgURL = data.poster_path;
-            const movieImg = document.createElement('img')
-            movieImg.setAttribute("src", "https://www.themoviedb.org/t/p/w500" + imgURL);
-            console.log(movieImg);
-            moviePicContainer.appendChild(movieImg);
-
-
-
-
+        const imgURL = data.poster_path;
+        let movieImg = document.createElement('img')
+        moviePicContainer.appendChild(movieImg);
+        movieImg.setAttribute("src", "https://www.themoviedb.org/t/p/w500" + imgURL);
+        console.log(movieImg);
+        
+        
+        
+        
             
     })
 
 }
 //display inforamtion using ID's
+function generates(){
+    const requestIngredientUrl = 'https://www.themealdb.com/api/json/v1/1/random.php';
 
 //fetch the food.
+  fetch(requestIngredientUrl)
+  .then(function(response){
+      return response.json();
+  })
+  // display information using ID's
+  .then(function(ingredientData){
+console.log(ingredientData.meals[0]);
+//Title 
+const recipetitle = ingredientData.meals[0].strMeal;
 
-// display information using ID's
+comboTitle.textContent = recipetitle;
+//Image
+const recipeImage = ingredientData.meals[0].strMealThumb;
 
-// add event listener / href to re-roll
+const imgTag = document.createElement("img");
+imgTag.src = recipeImage;
+comboPic.appendChild(imgTag);
+ 
+//Ingredients 
+    for (let i = 1; i < 21; i++) {
+        const ingredientList = ingredientData.meals[0]["strIngredient" + i];
+
+        if (ingredientList != "") {
+            // complete for loop ( remove extra 'dot points' )
+            const list = document.createElement("li");
+            list.textContent = ingredientList
+            ingredients.appendChild(list);
+        }
+    }
+ })
+
+ }
