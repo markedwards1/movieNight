443bb49a78f4613696fe61e07a1acbf93399a28a
njenkins2727 <njenkins2727@gmail.com>
2022-06-29 18:37:38 +0800

    added localStorage

--- assets/script.js
+++ assets/script.js
@@ -8,31 +8,60 @@ const ingredients = document.getElementById('ingredients');
 
 const generate = document.getElementById('generate');
 
- generate.addEventListener('click', generates());
 
-//create random number
-const number = 777281;
-const movieNumber = Math.floor(Math.random()* number);
-//console.log(movieNumber);
+generate.addEventListener('click', function(event){
+    event.preventDefault();
+    getApiMovie();
+    getFoodApi();
+});
+
 getApiMovie();
+getFoodApi();
+
+
+function getItems(key){
+    return JSON.parse(localStorage.getItem(key)) || []; //Notes: gets array of items 
+}
+
+function addItemToStorage(key, itemName){
+    //  goal: no longer than 5 items in localStorage 
+
+    // get existing items from local storage
+    const existingItems = getItems(key); //Notes: all items will be in this variable 
+
+    // add 'name' to them
+    existingItems.push(itemName); //adds new name to the array we have gotten 
+
+    // if total length is > 5
+    if(existingItems.length >5 ){ 
+        // remove the first item
+        existingItems.splice(0, 1); // deletes first index by 1 
+    }
+
+    // resave 
+    localStorage.setItem(key, JSON.stringify(existingItems)); 
+}
 
 //fetch movie
 //insert random number into url to search the movies. 
-function getApiMovie(){
+function getApiMovie() {
+    const number = 777281;
+    const movieNumber = Math.floor(Math.random() * number);
     let requestUrl = 'https://api.themoviedb.org/3/movie/' + movieNumber + '?api_key=417ba82f420aac26f214a4ce75d520d6';
-   console.log(requestUrl);
-   fetch(requestUrl)
-    .then(function (response){
-        return response.json();
-    })
-    .then(function (data){
-        console.log(data);
-    
-       movieTitle.textContent = data.original_title;
+    console.log(requestUrl);
+    fetch(requestUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            moviePicContainer.textContent = "";
+            movieTitle.textContent = data.original_title;
 
-       review.textContent = data.overview;
+            review.textContent = data.overview;
 
-        
+
+
 
 
 
@@ -44,45 +73,57 @@ function getApiMovie(){
 
 
 
+            //localStorage for movie
+            const movieName = data.original_title
+            addItemToStorage('movies', movieName);
 
-            
-    })
+        })
 
 }
 //display inforamtion using ID's
-function generates(){
+function getFoodApi() {
     const requestIngredientUrl = 'https://www.themealdb.com/api/json/v1/1/random.php';
 
-//fetch the food.
-  fetch(requestIngredientUrl)
-  .then(function(response){
-      return response.json();
-  })
-  // display information using ID's
-  .then(function(ingredientData){
-console.log(ingredientData.meals[0]);
-//Title 
-const recipetitle = ingredientData.meals[0].strMeal;
+    //fetch the food.
+    fetch(requestIngredientUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        // display information using ID's
+        .then(function (ingredientData) {
 
-comboTitle.textContent = recipetitle;
-//Image
-const recipeImage = ingredientData.meals[0].strMealThumb;
+            comboPic.textContent = "";
+            ingredients.textContent ="";
 
-const imgTag = document.createElement("img");
-imgTag.src = recipeImage;
-comboPic.appendChild(imgTag);
- 
-//Ingredients 
-    for (let i = 1; i < 21; i++) {
-        const ingredientList = ingredientData.meals[0]["strIngredient" + i];
+            console.log(ingredientData.meals[0]);
+            //Title 
+            const recipetitle = ingredientData.meals[0].strMeal;
 
-        if (ingredientList != "") {
-            // complete for loop ( remove extra 'dot points' )
-            const list = document.createElement("li");
-            list.textContent = ingredientList
-            ingredients.appendChild(list);
-        }
-    }
- })
+            comboTitle.textContent = recipetitle;
+            //Image
+            const recipeImage = ingredientData.meals[0].strMealThumb;
 
- }
+            const imgTag = document.createElement("img");
+            imgTag.src = recipeImage;
+            comboPic.appendChild(imgTag);
+
+            //Ingredients 
+            for (let i = 1; i < 21; i++) {
+                const ingredientList = ingredientData.meals[0]["strIngredient" + i];
+
+                if (ingredientList != "") {
+                    // complete for loop ( remove extra 'dot points' )
+                    const list = document.createElement("li");
+                    list.textContent = ingredientList
+                    ingredients.appendChild(list);
+                }
+            }
+
+           
+            addItemToStorage('foods', recipetitle);
+
+  
+        })
+}
+
+
